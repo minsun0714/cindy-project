475c879b3add772b8840d3e3cd660d7fe7cb09a7
jasmine0714 <woooooooow22@gmail.com>
2024-05-16 20:34:24 +0900

    🔥 불필요한 import 제거

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,13 +3,12 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule,
     JwtModule.registerAsync({
-      imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
         secret: config.get<string>('JWT_SECRET'),
