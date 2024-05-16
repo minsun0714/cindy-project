b6fc58ad80c6364a6af6615ddf959f98618a8147
jasmine0714 <woooooooow22@gmail.com>
2024-05-16 18:50:11 +0900

    🚧 signin 함수 호출 시 메모리에 저장된 user와 user가 보낸 비밀번호를 return하는 것까지 구현

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
-
+import { UsersService } from '../users/users.service';
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async signIn(username: string, pass: string): Promise<any> {
+    const user = await this.usersService.getUser(username);
+    console.log('📢[auth.service.ts:9]: user: ', user);
+    return { user, pass };
+  }
+}
