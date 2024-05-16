9718b3a13f31783c663ba8fd6b28bf6454b44a8f
jasmine0714 <woooooooow22@gmail.com>
2024-05-16 15:02:08 +0900

    ✨ SideBar 추가

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom"
 import Header from "./Header"
+import SideBar from "./SideBar"
 
 function Layout() {
 	return (
 		<div>
 			<Header />
+			<SideBar />
 			<main>
 				<Outlet />
 			</main>
---
+++ src/SideBar.tsx
@@ -0,0 +1,11 @@
+const SideBar = () => {
+	return (
+		<aside className="w-1/5 min-h-screen border bg-gray-light border-gray-light">
+			<ul>
+				<li>1보다 큰지 확인</li>
+			</ul>
+		</aside>
+	)
+}
+
+export default SideBar
