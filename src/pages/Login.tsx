825319e9be070fc70a86718f7d2e17e89eafd6c3
jasmine0714 <woooooooow22@gmail.com>
2024-05-16 16:52:53 +0900

    💄 로그인 버튼 text-white 적용

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -6,8 +6,8 @@ const Login = () => {
 			<form className="flex flex-col gap-y-4">
 				<Input label={"아이디"} />
 				<Input label={"비밀번호"} />
-				<button className="h-12 border rounded-l border-mint bg-mint">
-					login
+				<button className="h-12 text-white border rounded-l border-mint bg-mint">
+					로그인
 				</button>
 			</form>
 		</div>
