8388bffa96c81b3c13b8ee8d502049fdae4e60ac
jasmine0714 <woooooooow22@gmail.com>
2024-05-16 15:58:23 +0900

    💄 햄버거 cursor-pointer 적용

--- src/Header.tsx
+++ src/Header.tsx
@@ -4,10 +4,10 @@ import useSideBarStore from "./store/SideBarStore"
 const Header = () => {
 	const { setIsOpen } = useSideBarStore()
 	return (
-		<header className="flex items-center justify-start h-16 border border-mint bg-mint">
+		<header className="flex items-center justify-start w-screen h-16 border border-mint bg-mint">
 			<img
 				src={HamburgerImg}
-				className="h-8 ml-2"
+				className="h-8 ml-2 cursor-pointer"
 				onClick={() => setIsOpen()}
 			/>
 		</header>
