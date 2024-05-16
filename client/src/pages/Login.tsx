cfede255b1a4ae0b14a571410ad4c8e7f3fc8457
jasmine0714 <woooooooow22@gmail.com>
2024-05-16 21:40:50 +0900

    ✨ 입력값이 store에 저장되도록 구현

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,11 +1,25 @@
+import useAuthStore from "../store/AuthStore"
 import Input from "./components/Input"
 
 const Login = () => {
+	const { userId, setUserId, password, setPassword } = useAuthStore()
 	return (
 		<div>
 			<form className="flex flex-col gap-y-4">
-				<Input label={"아이디"} />
-				<Input label={"비밀번호"} />
+				<Input
+					label={"아이디"}
+					value={userId}
+					onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+						setUserId(e.currentTarget.value)
+					}
+				/>
+				<Input
+					label={"비밀번호"}
+					value={password}
+					onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+						setPassword(e.currentTarget.value)
+					}
+				/>
 				<button className="h-12 text-white border rounded-l border-mint bg-mint">
 					로그인
 				</button>
--- client/src/pages/components/Input.tsx
+++ client/src/pages/components/Input.tsx
@@ -1,12 +1,18 @@
 type InputType = {
 	label: string
+	value: string
+	onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-const Input = ({ label }: InputType) => {
+const Input = ({ label, value, onChange }: InputType) => {
 	return (
 		<div className="flex flex-col items-start justify-start text-gray">
 			<label>{label}</label>
-			<input className="h-12 p-2 border-4 rounded-l w-60 border-mint focus:outline-none" />
+			<input
+				className="h-12 p-2 border-4 rounded-l w-60 border-mint focus:outline-none"
+				value={value}
+				onChange={onChange}
+			/>
 		</div>
 	)
 }
