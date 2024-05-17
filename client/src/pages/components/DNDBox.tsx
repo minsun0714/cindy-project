700030f02f5646f0450a9dcb0570b18d42e95e1b
jasmine0714 <woooooooow22@gmail.com>
2024-05-17 15:29:28 +0900

    ✨ 엑셀 업로드 및 읽기 구현

--- client/src/pages/components/DNDBox.tsx
+++ client/src/pages/components/DNDBox.tsx
@@ -1,9 +1,49 @@
+import * as XLSX from "xlsx"
+
 const DNDBox = () => {
+	const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const file = event.target.files ? event.target.files[0] : null
+		if (!file) {
+			console.log("No file selected.")
+			return
+		}
+		console.log(file.name)
+
+		const reader = new FileReader()
+
+		reader.onload = (e: ProgressEvent<FileReader>) => {
+			if (!e.target || !e.target.result) {
+				console.error("Failed to read file.")
+				return
+			}
+			if (typeof e.target.result === "string") {
+				return
+			}
+			const data = new Uint8Array(e.target.result)
+			const workbook = XLSX.read(data, { type: "array" })
+			const worksheet = workbook.Sheets[workbook.SheetNames[0]]
+			const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 })
+			console.log(jsonData)
+		}
+
+		reader.readAsArrayBuffer(file)
+	}
+
 	return (
 		<div className="flex items-center justify-center w-64 h-56 border-4 rounded-2xl border-mint">
-			<button className="w-10 h-10 text-white border rounded-full border-mint bg-mint">
+			<input
+				className="hidden"
+				id="fileInput"
+				type="file"
+				accept=".xlsx, .xls, .csv"
+				onChange={handleFileUpload}
+			/>
+			<label
+				htmlFor="fileInput"
+				className="w-10 h-10 text-white border rounded-full cursor-pointer border-mint bg-mint"
+			>
 				+
-			</button>
+			</label>
 		</div>
 	)
 }
